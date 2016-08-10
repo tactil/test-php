3ae1141dc7dca66a801697e19f0993556d0f7d64
tactil <github@tactil.org>
2016-08-10 21:09:51 +0200

    Create version.php

---
+++ version.php
@@ -0,0 +1,10 @@
+<?php
+string phpversion ([ string $extension ] )
+
+
+/*A FAIRE POUR MISE EN FONCTIONNEMENT
+Changer nom de fichier en ligne13 : http://parisrentaparts.fr/pra/bookedday_2014.xml
+et en ligne 68 : $fp = fopen('bookedday_2014.xml', 'w');
+*/
+
+?>
